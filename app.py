e8a212ec8704bc210e7f2229097bf3b59d0b29ea
gorret golden <nabatanzigorret143@gmail.com>
2022-04-02 12:40:22 +0300

    heroku

---
+++ app.py
@@ -0,0 +1,43 @@
+import connexion
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+
+# # Create the Connexion application instance
+# app = connexion.App(__name__, specification_dir="./")
+
+# # Read the swagger.yml file to configure the endpoints
+# app.add_api('swagger.yml')
+
+# # Get the underlying Flask app instance
+# application = app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
+
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return "page"
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
